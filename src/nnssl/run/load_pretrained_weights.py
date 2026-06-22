29439dce241fe2f6945371f4973283b2b8d01c36
Andres Martinez Mora <andres2631996@hotmail.com>
2026-06-22 17:39:18 +0200

    Include pretraining with additional vessel distance map channel

--- src/nnssl/run/load_pretrained_weights.py
+++ src/nnssl/run/load_pretrained_weights.py
@@ -4,23 +4,10 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 
 def load_pretrained_weights(network, fname, verbose=False):
-    """
-    Transfers all weights between matching keys in state_dicts. matching is done by name and we only transfer if the
-    shape is also the same. Segmentation layers (the 1x1(x1) layers that produce the segmentation maps)
-    identified by keys ending with '.seg_layers') are not transferred!
-
-    If the pretrained weights were obtained with a training outside nnU-Net and DDP or torch.optimize was used,
-    you need to change the keys of the pretrained state_dict. DDP adds a 'module.' prefix and torch.optim adds
-    '_orig_mod'. You DO NOT need to worry about this if pretraining was done with nnU-Net as
-    nnUNetTrainer.save_checkpoint takes care of that!
-
-    """
     saved_model = torch.load(fname, weights_only=True)
-    pretrained_dict = saved_model['network_weights']
+    pretrained_dict = saved_model["network_weights"]
 
-    skip_strings_in_pretrained = [
-        '.seg_layers.',
-    ]
+    skip_strings_in_pretrained = [".seg_layers."]
 
     if isinstance(network, DDP):
         mod = network.module
@@ -30,37 +17,52 @@ def load_pretrained_weights(network, fname, verbose=False):
         mod = mod._orig_mod
 
     model_dict = mod.state_dict()
-    # verify that all but the segmentation layers have the same shape
-    for key, _ in model_dict.items():
-        if all([i not in key for i in skip_strings_in_pretrained]):
-            assert key in pretrained_dict, \
-                f"Key {key} is missing in the pretrained model weights. The pretrained weights do not seem to be " \
-                f"compatible with your network."
-            assert model_dict[key].shape == pretrained_dict[key].shape, \
-                f"The shape of the parameters of key {key} is not the same. Pretrained model: " \
-                f"{pretrained_dict[key].shape}; your network: {model_dict[key]}. The pretrained model " \
-                f"does not seem to be compatible with your network."
 
-    # fun fact: in principle this allows loading from parameters that do not cover the entire network. For example pretrained
-    # encoders. Not supported by this function though (see assertions above)
+    def _adapt_to_target(pretrained, target):
+        """Copy pretrained into the matching corner of a (same- or larger-shaped)
+        target tensor, zero-initializing any new entries. Returns None if the
+        target is smaller in any dim (genuinely incompatible)."""
+        if pretrained.dim() != target.dim():
+            return None
+        if any(p > t for p, t in zip(pretrained.shape, target.shape)):
+            return None
+        new_w = torch.zeros_like(target)
+        slices = tuple(slice(0, p) for p in pretrained.shape)
+        new_w[slices] = pretrained.to(new_w)
+        return new_w
 
-    # commenting out this abomination of a dict comprehension for preservation in the archives of 'what not to do'
-    # pretrained_dict = {'module.' + k if is_ddp else k: v
-    #                    for k, v in pretrained_dict.items()
-    #                    if (('module.' + k if is_ddp else k) in model_dict) and
-    #                    all([i not in k for i in skip_strings_in_pretrained])}
+    adapted_dict = {}
+    for k, v in pretrained_dict.items():
+        if k not in model_dict or any(s in k for s in skip_strings_in_pretrained):
+            continue
+        target = model_dict[k]
+        if v.shape == target.shape:
+            adapted_dict[k] = v
+        else:
+            nw = _adapt_to_target(v, target)
+            if nw is not None:
+                adapted_dict[k] = nw
+                print(
+                    f"Adapted '{k}': {tuple(v.shape)} -> {tuple(target.shape)} "
+                    f"(pretrained copied, new entries zero-initialized)"
+                )
+            else:
+                print(
+                    f"Skipping '{k}': incompatible shape "
+                    f"{tuple(v.shape)} vs {tuple(target.shape)}"
+                )
 
-    pretrained_dict = {k: v for k, v in pretrained_dict.items()
-                       if k in model_dict.keys() and all([i not in k for i in skip_strings_in_pretrained])}
+    model_dict.update(adapted_dict)
 
-    model_dict.update(pretrained_dict)
-
-    print("################### Loading pretrained weights from file ", fname, '###################')
+    print(
+        "################### Loading pretrained weights from file ",
+        fname,
+        "###################",
+    )
     if verbose:
-        print("Below is the list of overlapping blocks in pretrained model and nnUNet architecture:")
-        for key, value in pretrained_dict.items():
-            print(key, 'shape', value.shape)
+        print("Overlapping / adapted blocks:")
+        for key, value in adapted_dict.items():
+            print(key, "shape", tuple(value.shape))
         print("################### Done ###################")
+
     mod.load_state_dict(model_dict)
-
-
--- src/nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
+++ src/nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
@@ -2105,6 +2105,215 @@ class BaseMAETrainer_recDistMap(BaseMAETrainer):
         return {"loss": l.detach().cpu().numpy()}
     
 
+class BaseMAETrainer_recDistMap2chan(BaseMAETrainer):
+
+    def __init__(
+        self,
+        plan: Plan,
+        configuration_name: str,
+        fold: int,
+        pretrain_json: dict,
+        device: torch.device = torch.device("cuda"),
+    ):
+        # plan.configurations[configuration_name].batch_size = 1
+        super().__init__(plan, configuration_name, fold, pretrain_json, device)
+        self.config_plan.patch_size = (160, 160, 160)
+        self.mask_percentage: float = 0.75
+        self.num_input_channels = 2 # Set a second input channel for distance map inputs
+
+        self.im_output_folder = os.path.join(self.output_folder, "img_log")
+        os.makedirs(self.im_output_folder, exist_ok=True)
+        self.save_imgs_every_n_epochs = 200
+
+        self.num_output_channels = 2 # Set a second channel for distance map estimation
+
+    @staticmethod
+    def mask_creation(
+        segm: torch.Tensor,
+        batch_size: int,
+        patch_size: tuple[int, int, int],
+        mask_percentage: float,
+        rng_seed: int | None = None,
+        block_size: int = 16,
+    ) -> torch.Tensor:
+        """
+        Creates a masking tensor with 1s (indicating no masking) and 0s (indicating masking).
+        The mask has to be of same size like the input data (batch_size, 1, x, y, z).
+
+        :param segm: segmentation to bias the MAE masking process
+        :param batch_size: batch size during training
+        :param patch_size: The 3D shape information for the input patch.
+        :param mask_percentage: percentage of the patch that should be masked
+        :param block_size: size of the blocks that should be masked
+        :return:
+        """
+
+        sparsity_factor = mask_percentage
+        mask = [
+            create_blocky_mask_segm(seg=segm[i], tensor_size=patch_size, block_size=block_size, sparsity_factor=sparsity_factor)
+            for i in range(batch_size)
+        ]
+        mask = torch.stack(mask)[:, None, ...]  # Add channel dimension
+        return mask
+
+
+    def get_dataloaders(self):
+        patch_size = self.config_plan.patch_size
+        (
+            rotation_for_DA,
+            do_dummy_2d_data_aug,
+            initial_patch_size,
+            mirror_axes,
+        ) = configure_rotation_dummyDA_mirroring_and_inital_patch_size(patch_size)
+        if do_dummy_2d_data_aug:
+            self.print_to_log_file("Using dummy 2D data augmentation")
+
+        # ------------------------ Training data augmentations ----------------------- #
+        tr_transforms = self.get_training_transforms(
+            patch_size,
+            rotation_for_DA,
+            mirror_axes,
+            do_dummy_2d_data_aug,
+            order_resampling_data=3,
+            order_resampling_seg=1,
+            use_mask_for_norm=self.config_plan.use_mask_for_norm,
+        )
+
+        # ----------------------- Validation data augmentations ---------------------- #
+        val_transforms = self.get_validation_transforms()
+
+        dl_tr, dl_val = self.get_dist_dataloaders(initial_patch_size)
+
+        allowed_num_processes = get_allowed_n_proc_DA()
+        if allowed_num_processes == 0:
+            mt_gen_train = SingleThreadedAugmenter(dl_tr, tr_transforms)
+            mt_gen_val = SingleThreadedAugmenter(dl_val, val_transforms)
+        else:
+            mt_gen_train = LimitedLenWrapper(
+                self.num_iterations_per_epoch,
+                data_loader=dl_tr,
+                transform=tr_transforms,
+                num_processes=allowed_num_processes,
+                num_cached=6,
+                seeds=None,
+                pin_memory=self.device.type == "cuda",
+                wait_time=0.02,
+            )
+            mt_gen_val = LimitedLenWrapper(
+                self.num_val_iterations_per_epoch,
+                data_loader=dl_val,
+                transform=val_transforms,
+                num_processes=max(1, allowed_num_processes // 2),
+                num_cached=3,
+                seeds=None,
+                pin_memory=self.device.type == "cuda",
+                wait_time=0.02,
+            )
+        return mt_gen_train, mt_gen_val
+    
+
+    def build_loss(self):
+        """
+        This is where you build your loss function. You can use anything from torch.nn here.
+        In general the MAE losses are only applied on regions where the mask is 0.
+
+        :return:
+        """
+        return MAEMSELoss_recDistMap()
+
+    def train_step(self, batch: dict) -> dict:
+        data = batch["data"]
+        dist = data[:, -1].to(self.device, non_blocking=True)
+        dist = dist.unsqueeze(1)
+        data = data.to(self.device, non_blocking=True)
+        #data = data.unsqueeze(1)
+        anat = batch["seg"].to(self.device, non_blocking=True)
+
+        # We use the self.batch_size as it is not identical with the plan batch_size in ddp cases.
+        mask = self.mask_creation(
+            anat, self.batch_size, self.config_plan.patch_size, self.mask_percentage
+        ).to(self.device, non_blocking=True)
+        # Make the mask the same size as the data
+        rep_D, rep_H, rep_W = (
+            data.shape[2] // mask.shape[2],
+            data.shape[3] // mask.shape[3],
+            data.shape[4] // mask.shape[4],
+        )
+        mask = (
+            mask.repeat_interleave(rep_D, dim=2)
+            .repeat_interleave(rep_H, dim=3)
+            .repeat_interleave(rep_W, dim=4)
+        )
+
+        masked_data = data * mask
+
+        self.optimizer.zero_grad(set_to_none=True)
+        # Autocast is a little bitch.
+        # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
+        # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
+        # So autocast will only be active if we have a cuda device.
+        with (
+            autocast(self.device.type, enabled=True)
+            if self.device.type == "cuda"
+            else dummy_context()
+        ):
+            output = self.network(masked_data)
+            # del data
+            l = self.loss(output, data, dist, mask)
+
+        if self.grad_scaler is not None:
+            self.grad_scaler.scale(l).backward()
+            self.grad_scaler.unscale_(self.optimizer)
+            torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+            self.grad_scaler.step(self.optimizer)
+            self.grad_scaler.update()
+        else:
+            l.backward()
+            torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+            self.optimizer.step()
+
+        return {"loss": l.detach().cpu().numpy()}
+
+    def validation_step(self, batch: dict) -> dict:
+        data = batch["data"]
+        dist = data[:, -1].to(self.device, non_blocking=True)
+        dist = dist.unsqueeze(1)
+        data = data.to(self.device, non_blocking=True)
+        #data = data.unsqueeze(1)
+        anat = batch["seg"].to(self.device, non_blocking=True)
+
+        mask = self.mask_creation(
+            anat, self.batch_size, self.config_plan.patch_size, self.mask_percentage
+        ).to(self.device, non_blocking=True)
+        # Make the mask the same size as the data
+        rep_D, rep_H, rep_W = (
+            data.shape[2] // mask.shape[2],
+            data.shape[3] // mask.shape[3],
+            data.shape[4] // mask.shape[4],
+        )
+        mask = (
+            mask.repeat_interleave(rep_D, dim=2)
+            .repeat_interleave(rep_H, dim=3)
+            .repeat_interleave(rep_W, dim=4)
+        )
+
+        masked_data = data * mask
+
+        # Autocast is a little bitch.
+        # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
+        # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
+        # So autocast will only be active if we have a cuda device.
+        with (
+            autocast(self.device.type, enabled=True)
+            if self.device.type == "cuda"
+            else dummy_context()
+        ):
+            output = self.network(masked_data)
+            l = self.loss(output, data, dist, mask)
+
+        return {"loss": l.detach().cpu().numpy()}
+
+
 class BaseMAETrainer_weightedANAT_recDistMap(BaseMAETrainer_recDistMap):
     
     def build_loss(self):
@@ -2207,6 +2416,185 @@ class BaseMAETrainer_weightedANAT_recDistMap(BaseMAETrainer_recDistMap):
             l = self.loss(output, data, anat, dist, mask)
 
         return {"loss": l.detach().cpu().numpy()}
+
+
+class BaseMAETrainer_recDistMap2chan_warmup50ep(BaseMAETrainer_recDistMap2chan):
+
+    def __init__(
+        self,
+        plan: Plan,
+        configuration_name: str,
+        fold: int,
+        pretrain_json: dict,
+        device: torch.device,
+    ):
+
+        super(BaseMAETrainer_recDistMap2chan_warmup50ep, self).__init__(
+            plan,
+            configuration_name,
+            fold,
+            pretrain_json,
+            device,
+        )
+        # Fix the input patch size
+        self.config_plan.patch_size = (160, 160, 160)
+
+        ###settings taken from fabi
+        self.drop_path_rate = 0.2
+        self.attention_drop_rate = 0
+        self.grad_clip = 1
+        self.initial_lr = 3e-4
+        self.weight_decay = 5e-2
+        self.enable_deep_supervision = False
+        self.warmup_duration_whole_net = 50  # lin increase whole network
+        self.training_stage = None
+        self.num_output_channels = 2
+
+    def configure_optimizers(self, stage: str = "warmup_all"):
+        """
+        Two-stage training:
+        1) warmup_all  → linear LR warmup for `warmup_duration_whole_net` epochs
+        2) train       → poly LR decay starting AFTER warmup
+        """
+        assert stage in ["warmup_all", "train"]
+
+        # If already in this stage, return existing schedulers
+        if self.training_stage == stage:
+            return self.optimizer, self.lr_scheduler
+
+        # Select parameters (DDP-safe)
+        if isinstance(self.network, DDP):
+            params = self.network.module.parameters()
+        else:
+            params = self.network.parameters()
+
+        # -------------------------------
+        # 1) WARMUP STAGE
+        # -------------------------------
+        if stage == "warmup_all":
+            self.print_to_log_file("train whole net → WARMUP stage")
+
+            # fresh optimizer
+            optimizer = torch.optim.AdamW(
+                params,
+                lr=self.initial_lr,
+                weight_decay=self.weight_decay,
+                betas=(0.9, 0.98),
+                amsgrad=False,
+                fused=True,
+            )
+
+            # linear warmup → reaches initial_lr at warmup_duration_whole_net
+            lr_scheduler = Lin_incr_LRScheduler(
+                optimizer,
+                max_lr=self.initial_lr,
+                max_steps=self.warmup_duration_whole_net,
+            )
+
+            self.print_to_log_file(
+                f"[Warmup] Initialized at epoch {self.current_epoch}"
+            )
+
+        # -------------------------------
+        # 2) TRAIN STAGE (after warmup)
+        # -------------------------------
+        else:
+            self.print_to_log_file("train whole net → TRAIN stage")
+
+            # If transitioning warmup → train:
+            if self.training_stage == "warmup_all":
+                # keep optimizer from warmup (preserve momentum)
+                optimizer = self.optimizer
+                self.print_to_log_file(
+                    "Reusing optimizer from warmup (momentum preserved)."
+                )
+            else:
+                # If train is called directly (no warmup)
+                optimizer = torch.optim.AdamW(
+                    params,
+                    lr=self.initial_lr,
+                    weight_decay=self.weight_decay,
+                    betas=(0.9, 0.98),
+                    amsgrad=False,
+                    fused=True,
+                )
+
+            # Poly LR decay starting AFTER warmup duration
+            lr_scheduler = PolyLRScheduler_offset(
+                optimizer=optimizer,
+                initial_lr=self.initial_lr,
+                max_steps=self.num_epochs,
+                start_step=self.warmup_duration_whole_net,
+            )
+
+            self.print_to_log_file(f"[Train] Initialized at epoch {self.current_epoch}")
+
+        # Update state
+        self.training_stage = stage
+        empty_cache(self.device)
+
+        # Store inside object so next call knows what's already set
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
+
+        return optimizer, lr_scheduler
+
+    def on_train_epoch_start(self):
+        if self.current_epoch == 0:
+            self.optimizer, self.lr_scheduler = self.configure_optimizers("warmup_all")
+        elif self.current_epoch == self.warmup_duration_whole_net:
+            self.optimizer, self.lr_scheduler = self.configure_optimizers("train")
+
+        super().on_train_epoch_start()
+
+    def load_checkpoint(self, filename_or_checkpoint: Union[dict, str]) -> None:
+        if not self.was_initialized:
+            self.initialize()
+
+        if isinstance(filename_or_checkpoint, str):
+            checkpoint = torch.load(filename_or_checkpoint, map_location=self.device)
+        # if state dict comes from nn.DataParallel but we use non-parallel model here then the state dict keys do not
+        # match. Use heuristic to make it match
+        new_state_dict = {}
+        for k, value in checkpoint["network_weights"].items():
+            key = k
+            if key not in self.network.state_dict().keys() and key.startswith(
+                "module."
+            ):
+                key = key[7:]
+            new_state_dict[key] = value
+
+        self.my_init_kwargs = checkpoint["init_args"]
+
+        self.current_epoch = checkpoint["current_epoch"]
+        min_epoch = self.logger.load_checkpoint(checkpoint["logging"])
+        # Apparently the val log is not written correctly when we currently save the checkpoint.
+        self.current_epoch = min_epoch
+        self._best_ema = checkpoint["_best_ema"]
+
+        # messing with state dict naming schemes. Facepalm.
+        if self.is_ddp:
+            if isinstance(self.network.module, OptimizedModule):
+                self.network.module._orig_mod.load_state_dict(new_state_dict)
+            else:
+                self.network.module.load_state_dict(new_state_dict)
+        else:
+            if isinstance(self.network, OptimizedModule):
+                self.network._orig_mod.load_state_dict(new_state_dict)
+            else:
+                self.network.load_state_dict(new_state_dict)
+
+        # it's fine to do this every time we load because configure_optimizers will be a no-op if the correct optimizer
+        # and lr scheduler are already set up
+        if self.current_epoch < self.warmup_duration_whole_net:
+            self.optimizer, self.lr_scheduler = self.configure_optimizers("warmup_all")
+        else:
+            self.optimizer, self.lr_scheduler = self.configure_optimizers("train")
+
+        self.optimizer.load_state_dict(checkpoint["optimizer_state"])
+        if self.grad_scaler is not None:
+            if checkpoint["grad_scaler_state"] is not None:
+                self.grad_scaler.load_state_dict(checkpoint["grad_scaler_state"])
 
 
 class BaseMAETrainer_recDistMap_warmup50ep(BaseMAETrainer_recDistMap):
